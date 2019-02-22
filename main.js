6ab5fa0218dc6499a2e4a9038542975bd8e4b09e
npatel023 <npatel023@gmail.com>
2019-02-21 20:10:57 -0600

    working on login page

---
+++ main.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+
+let win;
+
+const { app, BrowserWindow } = require('electron');
+
+require('electron-reload')(__dirname);
+
+function createWindow () {
+    win = new BrowserWindow({ width: 800, height: 600 });
+    // win.loadFile('index.html');
+
+    win.loadURL(`file://${__dirname}/index.html`);
+
+    //win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
